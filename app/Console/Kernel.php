cf791c27f89b66228d7990da94095cc1ea95f8d6
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-02-16 22:45:56 +0800

    add schedule for books:download command

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\DownloadBooks;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -25,7 +26,7 @@ final class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        //
+        $schedule->command(DownloadBooks::class)->dailyAt('03:03');
     }
 
     /**
