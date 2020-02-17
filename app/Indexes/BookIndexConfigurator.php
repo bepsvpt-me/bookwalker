e0bc76a72a1b67c42b318e915be07b3216c7faae
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-02-17 19:42:28 +0800

    add simaple and traditional chinese converter

--- app/Indexes/BookIndexConfigurator.php
+++ app/Indexes/BookIndexConfigurator.php
@@ -13,6 +13,34 @@ final class BookIndexConfigurator extends IndexConfigurator
      * @var array
      */
     protected $settings = [
-        //
+        'analysis' => [
+            'analyzer' => [
+                'tsconvert' => [
+                    'tokenizer' => 'tsconvert',
+                ],
+            ],
+            'tokenizer' => [
+                'tsconvert' => [
+                    'type' => 'stconvert',
+                    'delimiter' => '#',
+                    'keep_both' => false,
+                    'convert_type' => 't2s',
+                ],
+            ],
+            'filter' => [
+                'tsconvert' => [
+                    'type' => 'stconvert',
+                    'delimiter' => '#',
+                    'keep_both' => false,
+                    'convert_type' => 't2s',
+                ],
+            ],
+            'char_filter' => [
+                'tsconvert' => [
+                    'type' => 'stconvert',
+                    'convert_type' => 't2s',
+                ],
+            ],
+        ],
     ];
 }
