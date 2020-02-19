49be20129e3a9d9262d4efb7c3fe0168ee32eeab
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-02-19 21:10:57 +0800

    update official link text

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -186,7 +186,7 @@
               rel="noopener noreferrer"
               target="_blank"
             >
-              <small>前往書籍 BookWalker 官方頁面</small>
+              <small>前往此書籍 BOOKWALKER 官方頁面</small>
 
               <svg viewBox="0 0 24 24">
                 <path fill="currentColor" d="M14,3V5H17.59L7.76,14.83L9.17,16.24L19,6.41V10H21V3M19,19H5V5H12V3H5C3.89,3 3,3.9 3,5V19A2,2 0 0,0 5,21H19A2,2 0 0,0 21,19V12H19V19Z" />
