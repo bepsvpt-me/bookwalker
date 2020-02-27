c99981d9364c90c449d9be9f6ba38e15c9001391
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-02-27 21:32:32 +0800

    add bookwalker config

---
+++ config/bookwalker.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+
+    'creators' => [
+
+        'author', // 作者
+
+        'writer', // 原著
+
+        'character_designer', // 角色設定
+
+        'illustrator', // 插畫
+
+        'translator', // 譯者
+
+        'cartoonist', // 漫畫
+
+    ],
+
+];
