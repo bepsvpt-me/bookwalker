d30f30c73f8a2393cf56901b70b9a319fd0e2b6f
bepsvpt <og7lsrszah6y3lz@infinitefa.email>
2020-02-27 22:36:30 +0800

    fix url transform

--- resources/views/components/books.blade.php
+++ resources/views/components/books.blade.php
@@ -57,7 +57,7 @@
 
                   <a
                     class="card-link"
-                    href="{{ route(\Illuminate\Support\Str::slug(substr($key, 0, -1)), ['name' => $creator->name]) }}"
+                    href="{{ route(\Illuminate\Support\Str::kebab(substr($key, 0, -1)), ['name' => $creator->name]) }}"
                   >
                     {{ $creator->name }}
                   </a>
